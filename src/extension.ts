7cfef555b1feb154519d7b87678bd5a9d968cde6
pfe171 <pureforeverearth@outlook.jp>
2024-10-11 17:54:14 +0900

    Add hover to open formula editor

--- src/extension.ts
+++ src/extension.ts
@@ -5,27 +5,43 @@ import * as vscode from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "formula-editor-vscode" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "formula-editor-vscode.helloWorld",
+  const openEditor = vscode.commands.registerCommand(
+    "formula-editor-vscode.editor",
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from formula-editor-vscode!"
+      const panel = vscode.window.createWebviewPanel(
+        "formulaEditor", // Identifies the type of the webview. Used internally
+        "Formula Editor", // Title of the panel displayed to the user
+        vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
       );
     }
   );
 
-  context.subscriptions.push(disposable);
+  vscode.languages.registerHoverProvider(
+    "markdown",
+    new (class implements vscode.HoverProvider {
+      provideHover(
+        _document: vscode.TextDocument,
+        _position: vscode.Position,
+        _token: vscode.CancellationToken
+      ): vscode.ProviderResult<vscode.Hover> {
+        const commentCommandUri = vscode.Uri.parse(
+          `command:formula-editor-vscode.editor`
+        );
+        const contents = new vscode.MarkdownString(
+          `[Edit this formula](${commentCommandUri})`
+        );
+
+        contents.isTrusted = true;
+
+        return new vscode.Hover(contents);
+      }
+    })()
+  );
+
+  context.subscriptions.push(openEditor);
 }
 
 // This method is called when your extension is deactivated
